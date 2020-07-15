05bc2f696c3d09be34c0858c1ab42350d03265c4
Kevin Boston <kbos22@gmail.com>
2020-07-15 12:31:36 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,15 @@
 class Show < ActiveRecord::Base 
+  def highest_rating
+    self.maximum(:rating)
+  end
+  def most_popular_show
+    self.
+  end
   
+  def lowest_rating
+    self.minimum(:rating)
+  end
+  def ratings_sum
+    self.sum(:rating)
+  end
 end
